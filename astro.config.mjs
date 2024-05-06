80df6e06b3a4bf476df36a6dc1fb11bed9ea5657
Marcelo Mossian <mmossian@gmail.com>
2024-05-06 10:25:08 -0300

    Astro Blog V1.0 - final

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from "astro/config";
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-	site: "https://663616eda93f24dc8dcbe835--mm-astro-blog.netlify.app",
+  site: "https://663616eda93f24dc8dcbe835--mm-astro-blog.netlify.app",
+  integrations: [preact()]
 });
